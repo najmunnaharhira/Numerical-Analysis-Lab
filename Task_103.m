4d2c42a4e7ce9c29104573d294fb5009e20f5174
Najmun Nahar Hira <107489391+najmunnaharhira@users.noreply.github.com>
2023-11-09 18:05:01 +0600

    Update Task_103.m

--- Task_103.m
+++ Task_103.m
@@ -1,20 +1,15 @@
 function retval = task_103 (input1, input2)
-
 function y = f(x)
     y = x.^2 - 4;
 end
-
 a = 0;
 b = 3;
 t = 1e-6;
-
 roots = [];
 iterations = 0;
-
 while (b - a) / 2 > t
     c = (a*f(b) - b*f(a)) / (f(b) - f(a));
     roots = [roots; c];
-
     if f(c) == 0
         break;
     elseif f(a) * f(c) < 0
@@ -22,15 +17,11 @@ while (b - a) / 2 > t
     else
         a = c;
     end
-
-    iterations = iterations + 1;
+   iterations = iterations + 1;
 end
-
 fprintf('%d iterations needed: %f\n', iterations, roots(end));
-
 x = linspace(0, 3, 100);
 y = f(x);
-
 figure;
 plot(x, y);
 hold on;
@@ -40,6 +31,4 @@ ylabel('function value');
 title('False-Position Method ');
 legend('function falues', 'Roots');
 grid on;
-
-
 endfunction
