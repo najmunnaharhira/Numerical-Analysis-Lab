69462657629841c88c74e2777a46de6c920cee71
Najmun Nahar Hira <107489391+najmunnaharhira@users.noreply.github.com>
2023-11-09 18:03:11 +0600

    Update Task_102.m

--- Task_102.m
+++ Task_102.m
@@ -1,20 +1,15 @@
 function retval = task_102 ()
-
 function y = f(x)
     y = x.^2 - 4;
 end
-
 a = 0;
 b = 3;
 t = 1e-6;
-
 roots = [];
 iterations = 0;
-
 while (b - a) / 2 > t
     c = (a + b) / 2;
     roots = [roots; c];
-
     if f(c) == 0
         break;
     elseif f(a) * f(c) < 0
@@ -22,15 +17,11 @@ while (b - a) / 2 > t
     else
         a = c;
     end
-
     iterations = iterations + 1;
 end
-
 fprintf('%d iterations needed: %f\n', iterations, roots(end));
-
  x = linspace(0, 3, 100);
  y = f(x);
-
  figure;
  plot(x, y);
 hold on;
@@ -40,7 +31,4 @@ ylabel('function values');
 title('Bisection Method');
 legend('function values ', 'Roots');
 grid on;
-
-
-
 endfunction
